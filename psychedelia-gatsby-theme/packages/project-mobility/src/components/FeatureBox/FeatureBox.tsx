db0838767ffdfcdf3899cb2c9fe652fa40d27e82
Pouya Ataei <pouya.ataei@vodafone.com>
2020-03-08 22:22:29 +1300

    feature box container

--- psychedelia-gatsby-theme/packages/project-mobility/src/components/FeatureBox/FeatureBox.tsx
+++ psychedelia-gatsby-theme/packages/project-mobility/src/components/FeatureBox/FeatureBox.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import Img from "gatsby-image";
 
+// Components
 import { StyledDivWrapper, H2, P } from "../Common";
 
-const FeatureBox = () => (
-  <StyledDivWrapper>
+
+const FeatureBox = () => {
+
+ 
+
+  return (
+    <StyledDivWrapper>
     <StyledDivWrapper></StyledDivWrapper>
     <StyledDivWrapper></StyledDivWrapper>
   </StyledDivWrapper>
-);
+  )
+ 
+}
 
 export default FeatureBox;
---
+++ psychedelia-gatsby-theme/packages/project-mobility/src/components/FeatureBoxContainer/FeatureBoxContainer.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+// Queries
+import useFeatureBox from "../../queries/useFeatureBox"
+
+const FeatureBoxContainer =()=> {
+
+    const featureBoxData = useFeatureBox()
+
+    return (<div>test</div>)
+}
+
+export default FeatureBoxContainer
